400693f9f2c8624fcd25dfcc6e3316a5ebf5b2a9
patryktest <patryktesting@gmail.com>
2013-12-06 15:11:10 +0100

    s

---
+++ gen/R.java.d
@@ -0,0 +1,9 @@
+C:\Users\patrik\cordova\hi\platforms\android\gen\com\example\hi\R.java \
+ : C:\Users\patrik\cordova\hi\platforms\android\res\drawable\icon.png \
+C:\Users\patrik\cordova\hi\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\patrik\cordova\hi\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\patrik\cordova\hi\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\patrik\cordova\hi\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\patrik\cordova\hi\platforms\android\res\values\strings.xml \
+C:\Users\patrik\cordova\hi\platforms\android\res\xml\config.xml \
+C:\Users\patrik\cordova\hi\platforms\android\bin\AndroidManifest.xml \
