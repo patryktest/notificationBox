e61ca9e84c57d5dc386433bf6e9a754ee9b53852
patryktest <patryktesting@gmail.com>
2013-12-06 13:10:06 +0100

    f

---
+++ www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+]
+});
---
+++ www/plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js
@@ -0,0 +1,88 @@
+cordova.define("de.appplant.cordova.plugin.local-notification.LocalNotification", function(require, exports, module) {/**
+ *  locale-notification.js
+ *  Cordova LocalNotification Plugin
+ *
+ *  Created by Sebastian Katzer (github.com/katzer) on 10/08/2013.
+ *  Copyright 2013 Sebastian Katzer. All rights reserved.
+ *  GPL v2 licensed
+ */
+
+var LocalNotification = function () {
+
+};
+
+LocalNotification.prototype = {
+    /**
+     * Fügt einen neuen Eintrag zur Registry hinzu.
+     *
+     * @param {Object} options
+     * @return {Number} Die ID der Notification
+     */
+    add: function (options) {
+        var defaults = {
+            date:       new Date(),
+            message:    '',
+            title:      '',
+            badge:      0,
+            id:         0,
+            repeat:     '',
+            background: undefined,
+            foreground: undefined
+        };
+
+        switch (device.platform) {
+            case 'Android':
+                defaults.icon = 'icon';
+                defaults.sound = 'TYPE_NOTIFICATION'; break;
+            case 'iOS':
+                defaults.sound = ''; break;
+            case 'WinCE': case 'Win32NT':
+                defaults.smallImage = null;
+                defaults.image = null;
+                defaults.wideImage = null;
+        };
+
+        var callbackFn = function (cmd) {
+            eval(cmd);
+        };
+
+        for (var key in defaults) {
+            if (options[key] !== undefined) {
+                defaults[key] = options[key];
+            }
+        }
+
+        if (defaults.id) {
+            defaults.id = defaults.id.toString();
+        }
+
+        if (typeof defaults.date == 'object') {
+            defaults.date = Math.round(defaults.date.getTime()/1000);
+        }
+
+        cordova.exec(callbackFn, null, 'LocalNotification', 'add', [defaults]);
+
+        return defaults.id;
+    },
+
+    /**
+     * Entfernt die angegebene Notification.
+     *
+     * @param {String} id
+     */
+    cancel: function (id) {
+        cordova.exec(null, null, 'LocalNotification', 'cancel', [id.toString()]);
+    },
+
+    /**
+     * Entfernt alle registrierten Notifications.
+     */
+    cancelAll: function () {
+        cordova.exec(null, null, 'LocalNotification', 'cancelAll', []);
+    }
+};
+
+var plugin = new LocalNotification();
+
+module.exports = plugin;
+});
