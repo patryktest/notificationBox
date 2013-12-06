8bc4f048e95532e67d9eef2c75f9528568df1389
patryktest <patryktesting@gmail.com>
2013-12-06 15:06:28 +0100

    s

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        window.plugin.notification.local.add({ message: 'Great app!' });
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
